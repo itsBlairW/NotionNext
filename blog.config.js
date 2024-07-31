ef6c08b29fbb7427a546297fb546cd53fca48252
itsBlairW <71138822+itsBlairW@users.noreply.github.com>
2024-07-31 01:18:38 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,11 +27,11 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Blair Wang', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 
-    `🇨🇳 ➡️ 🇨🇦
-    🎓 Bsc | Kingston | QU | 23'
-    🎓 MEng | London | UWO | 24'
+    `🇨🇳 ➡️ 🇨🇦 /n
+    🎓 Bsc | Kingston | QU | 23' /n
+    🎓 MEng | London | UWO | 24' /n
     🔧 Software engineer | Machine Learning Engineer | Data Engineer`, // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://itsblair.vercel.app/en', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blair, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
@@ -39,6 +39,7 @@ const BLOG = {
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://x.com/whitenox0917', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/itsBlairW', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_FACEBOOK: process.env.NEXT_PUBLIC_CONTACT_FACEBOOK || 'https://www.facebook.com/profile.php?id=100087989295204&mibextid=LQQJ4d', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/237507931?spm_id_from=333.1007.0.0', // B站主页
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || 'https://www.linkedin.com/in/jingyi-wang-a5341b261/', // 你的linkedIn 首页
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://www.instagram.com/blair_wangjy/?igsh=MTdzbGY3a21mNGQzdQ%3D%3D&utm_source=qr', // 您的instagram地址
   
