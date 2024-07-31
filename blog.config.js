194042bc424df586d514d82606ab5e4c0748c55b
itsBlairW <71138822+itsBlairW@users.noreply.github.com>
2024-07-31 00:57:30 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Blair Wang', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || "🇨🇳 ➡️ 🇨🇦, 🎓 Bsc | Kingston | QU | 23', 🎓 MEng | London | UWO | 24', 🔧 Software engineer | Machine Learning Engineer | Data Engineer", // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || "🇨🇳 ➡️ 🇨🇦\n 🎓 Bsc | Kingston | QU | 23'\n 🎓 MEng | London | UWO | 24'\n 🔧 Software engineer | Machine Learning Engineer | Data Engineer", // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blair, Blog', // 网站关键词 英文逗号隔开
 
